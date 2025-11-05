37febcc43dcbdedd7d730b9e4bed0293773834ce
Lukas <lukasz@spychala.dev>
2025-11-05 11:52:25 +0100

    add gitignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pycouchdb
 import json
+import credentials
 
 def connect_to_db(login, password, host):
     db_name = "bb-" + login
@@ -23,21 +24,21 @@ def dump_db_to_file(db, filename="db_dump.json"):
     with open(filename, "w") as f:
         json.dump(docs, f, indent=2)
 
-
-LOGIN = "85d9d216-26f7-4f69-8532-43a149b5e2e8"
-PASSWORD = "4e223655-cb57-4c98-a596-7d23b73e3219"
-HOST = "couch-prod-eu-2.budgetbakers.com"
-
-db = connect_to_db(LOGIN, PASSWORD, HOST)
-
-records = get_all_records(db.all())
-print_records(records)
-
-
-
-
 def main():
-    print("Hello from wallet-library!")
+
+    LOGIN = credentials.LOGIN
+    PASSWORD = credentials.PASSWORD
+    HOST = credentials.HOST
+
+    db = connect_to_db(LOGIN, PASSWORD, HOST)
+
+    records = get_all_records(db.all())
+    print_records(records)
+
+
+
+
+
 
 
 if __name__ == "__main__":
